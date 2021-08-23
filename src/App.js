84f318795713fd7460fba29ce53503587342a284
vishal00923 <vishalbg.mz@gmail.com>
2021-08-23 23:46:46 +0530

    🚧 (App, component) : Work in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/app.scss';
+import Player from './components/Player';
+import Song from './components/Song';
+import data from './util';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // State variables
+    const [songs, setSongs] = useState(data());
+    const [currentSong, setCurrentSong] = useState(songs[0]);
+    const [alt, setAlt] = useState(songs[0]);
+    return (
+        <div className="App">
+            <Song currentSong={currentSong} alt={alt} />
+            <Player />
+        </div>
+    );
 }
 
 export default App;
