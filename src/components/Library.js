b8931e195d5f3ed87c148b8fc5ac9f9d1b448eb6
vishal00923 <vishalbg.mz@gmail.com>
2021-08-25 19:52:51 +0530

    ✨ (Library, component) : Add song library

---
+++ src/components/Library.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import LibrarySong from './LibrarySong';
+
+const Library = ({ songs, setSongs, setCurrentSong, isPlaying, audioRef }) => {
+    return (
+        <div className="library">
+            <h2>Library</h2>
+            <div className="library-songs">
+                {songs.map((song) => (
+                    <LibrarySong
+                        song={song}
+                        setCurrentSong={setCurrentSong}
+                        songs={songs}
+                        setSongs={setSongs}
+                        id={song.id}
+                        key={song.id}
+                        isPlaying={isPlaying}
+                        audioRef={audioRef}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Library;
