0dba17356f03d5bdb8431220563bf42b4c980e48
vishal00923 <vishalbg.mz@gmail.com>
2021-08-25 23:58:42 +0530

    🔧 Add appdata

---
+++ src/appdata.js
@@ -0,0 +1,98 @@
+import { v4 as uuidv4 } from 'uuid';
+
+function chillHop() {
+    return [
+        {
+            id: uuidv4(),
+            cover: 'https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-1024x1024.jpg',
+            name: 'travelbag',
+            artist: 'mommy, Sleepy Fish',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=19058',
+            color: ['#5F92D3', '#C8CAC5'],
+            active: true,
+        },
+        {
+            id: uuidv4(),
+            cover: 'https://chillhop.com/wp-content/uploads/2021/07/034f52eeaeceb144cca67b0930d705d32fc39e37-1024x1024.jpg',
+            name: 'Memories Pt.1',
+            artist: 'Ruck P',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=21659',
+            color: ['#6B6883', '#D38E8F'],
+            active: false,
+        },
+        {
+            id: uuidv4(),
+            cover: 'https://chillhop.com/wp-content/uploads/2021/07/4163ebb931e06d4ee8eb184295c0246d4a5055f7-1024x1024.jpg',
+            name: 'Cabaña',
+            artist: 'Juan Rios',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=21647',
+            color: ['#F9BEB6', '#542E47'],
+            active: false,
+        },
+        {
+            id: uuidv4(),
+            cover: 'https://chillhop.com/wp-content/uploads/2021/07/935da7886600df5eeb2d3b13b12cf27ee8c6c700-1024x1024.jpg',
+            name: 'What If I Told You',
+            artist: 'Juan Rios',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=21649',
+            color: ['#FDD69A', '#D9B9A0'],
+            active: false,
+        },
+        {
+            id: uuidv4(),
+            cover: 'https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg',
+            name: 'Viatecture',
+            artist: 'Leavv, Makzo',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=20119',
+            color: ['#ADB754', '#A28ABA'],
+            active: false,
+        },
+        {
+            id: uuidv4(),
+            cover: 'https://chillhop.com/wp-content/uploads/2021/07/3b73a510169f14c4af83ac4016e809917412702b-1024x1024.jpg',
+            name: 'Vitamin D',
+            artist: 'fantompower',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=22790',
+            color: ['#D8B4D8', '#C5D8DF'],
+            active: false,
+        },
+        {
+            id: uuidv4(),
+            cover: 'https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-1024x1024.jpg',
+            name: 'Dawn',
+            artist: 'Toonorth',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=17941',
+            color: ['#50556B', '#354A65'],
+            active: false,
+        },
+        {
+            id: uuidv4(),
+            cover: 'https://chillhop.com/wp-content/uploads/2021/05/8ef1fa972003495d1ecfba6292116174e9c5d940-1024x1024.jpg',
+            name: 'Dreamstate',
+            artist: 'Toonorth',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=17947',
+            color: ['#96A6E3', '#2A4359'],
+            active: false,
+        },
+        {
+            id: uuidv4(),
+            cover: 'https://chillhop.com/wp-content/uploads/2021/05/94a210b97ac05e5f05e1ce784a1a9ecf90168d03-1024x1024.jpg',
+            name: 'Lavish',
+            artist: 'Toonorth',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=17933',
+            color: ['#454948', '#FF872F'],
+            active: false,
+        },
+        {
+            id: uuidv4(),
+            cover: 'https://chillhop.com/wp-content/uploads/2021/04/cb0cc6270d7f2e1bb13e44e8832bd5c9b2a61080-1024x1024.jpg',
+            name: 'Cruising',
+            artist: 'Evil Needle',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=17087',
+            color: ['#91726B', '#EC614E'],
+            active: false,
+        },
+    ];
+}
+
+export default chillHop;
