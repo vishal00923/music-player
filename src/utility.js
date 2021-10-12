f4c4b419a90cc67e7e1ea2ad6192520bc87439e9
vishal00923 <vishalbg.mz@gmail.com>
2021-10-12 20:43:38 +0530

    utility : Add

---
+++ src/utility.js
@@ -0,0 +1,15 @@
+// Specific exports
+
+export const playAudio = (isPlaying, audioRef) => {
+    if (isPlaying) {
+        const playPromise = audioRef.current.play();
+
+        if (playPromise !== undefined) {
+            playPromise
+                .then((audio) => {
+                    audioRef.current.play();
+                })
+                .catch((error) => console.log(error));
+        }
+    }
+};
